ad1b2bca3e081c7d43e5fa0295a803cafa6ead18
DimaJettixx <130287415+DimaJettixx@users.noreply.github.com>
2023-04-09 17:26:47 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+from PyQt5.QtWidgets import*
+
+app = QApplication([])
+okno = QWidget()
+
+main = QHBoxLayout()
+okno.setLayout(main)
+
+line1 = QVBoxLayout()
+line2 = QVBoxLayout()
+main.addLayout(line1)
+main.addLayout(line2)
+
+b1 = QPushButton('Дальше')
+tx1 = QLabel('результат')
+line1.addWidget(b1)
+line2.addWidget(tx1)
+
+tx1.hide()
+
+def next1():
+    tx1.show()
+    b1.hide()
+b1.clicked.connect(next1)
+
+okno.show()
+app.exec_()
